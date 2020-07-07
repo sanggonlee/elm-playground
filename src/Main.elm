68d659bf3b8577e7176e0f81bf1a18689cd8f5eb
sanggonlee <lee.sg16@gmail.com>
2020-07-06 21:01:43 -0400

    initial commit

---
+++ src/Main.elm
@@ -0,0 +1,30 @@
+module Main exposing (main, update, view)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+
+main: Program () Int Msg
+main =
+  Browser.sandbox { init = 0, update = update, view = view }
+
+type Msg = Increment | Decrement
+
+update: Msg -> Int -> Int
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+
+view: Int -> Html Msg
+view model =
+  div [ style "padding" "50px" ]
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
